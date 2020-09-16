7ff904ab4331541ac1676a49102f07c4475ed30c
Francisco Ponce <francisco.eugp@gmail.com>
2020-09-16 02:28:13 -0500

    Creation of cards and basic styles added

--- test/src/App.js
+++ test/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { CSSReset, Flex } from '@chakra-ui/core'
+import Card from './components/Card'
 
 const App = () => {
   return (
     <>
-      <Flex direction="column" align="center">
+      <Flex justify="center">
         <CSSReset />
+        <Card />
+        <Card />
       </Flex>
     </>
   )
