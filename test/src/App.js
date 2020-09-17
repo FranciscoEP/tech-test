421b1a4bc73b980c18067c919e87714e8ef5ba61
Francisco Ponce <francisco.eugp@gmail.com>
2020-09-17 02:07:57 -0500

    Img ready for testing environment

--- test/src/App.js
+++ test/src/App.js
@@ -1,15 +1,31 @@
 import React from 'react'
-import { CSSReset, Flex } from '@chakra-ui/core'
-import Card from './components/Card'
+import { CSSReset, Flex, Grid, Box, Stack } from '@chakra-ui/core'
+import BlueCard from './components/BlueCard'
+import GreenCard from './components/GreenCard'
+import Chart from './components/Chart'
+import ImgCard from './components/ImgCard'
 
 const App = () => {
   return (
     <>
-      <Flex justify="center">
-        <CSSReset />
-        <Card />
-        <Card />
-      </Flex>
+      <CSSReset />
+      <Box bg="gray.300">
+        {/* Dummie NavBar */}
+
+        <Box bg="cyan.800" py={4} color="white">
+          <Stack>
+            <Box maxW="700px" marginX="auto" fontSize="4xl" textAlign="center">
+              Components
+            </Box>
+          </Stack>
+        </Box>
+        <Grid templateColumns="repeat(2, 2fr)" gap={4} px={10} py={5}>
+          <BlueCard />
+          <GreenCard />
+          <Chart />
+          <ImgCard />
+        </Grid>
+      </Box>
     </>
   )
 }
